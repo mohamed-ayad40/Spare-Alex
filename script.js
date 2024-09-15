9e1abc38dcd78ddd57ec6999a94ab96e3d76a4d4
mohamed-ayad40 <138216055+mohamed-ayad40@users.noreply.github.com>
2024-09-15 23:40:55 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(() => {
+        document.getElementById("loader").style.display = "none";
+    }, 3000); // Hide loader after 3 seconds
+});
